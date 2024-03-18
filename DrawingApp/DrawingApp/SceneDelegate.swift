6f95b19c7aebeb466b505b32b2b91427df07bfcd
joho2022 <josama1997@naver.com>
2024-03-18 15:42:03 +0900

    feat: 아이패드 앱 프로젝트 생성, 팩토리방식으로 객체생성

---
+++ DrawingApp/DrawingApp/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  DrawingApp
+//
+//  Created by 조호근 on 3/18/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = DrawingViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+}
---
+++ DrawingApp/DrawingApp/ShapeProperties/Point.swift
@@ -0,0 +1,13 @@
+//
+//  Point.swift
+//  DrawingApp
+//
+//  Created by 조호근 on 3/18/24.
+//
+
+import Foundation
+
+struct Point {
+    var x: Double
+    var y: Double
+}
